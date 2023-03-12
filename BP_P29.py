10cca3b50a9cac694afa2b1950024ee50cf27f22
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 14:48:20 +0530

    Add files via upload

---
+++ BP_P29.py
@@ -0,0 +1,13 @@
+# Find min & max number from tuple
+
+# create tuple
+
+tuple1 = (12, 15, 25, 35, 20, 4, 23)
+
+print("Tuple is: ", tuple1)
+
+print("Minimum number is: ", min(tuple1))       # min(), function
+
+print("Maximum number is: ", max(tuple1))       # max(), function
+
+# Thanks for Watching
